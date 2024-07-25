fbd8cfaea94325467edf652ba455111f29b0d07d
Basheera <157336255+BASHEERATHAJUDEEN@users.noreply.github.com>
2024-07-25 10:13:21 +0530

    Create Practice-2_pr_1:Simple ASCII Art.java

---
+++ Practice/Practice-2_pr_1:Simple ASCII Art.java
@@ -0,0 +1,13 @@
+//problem - 1
+public class SmileyFace {
+    public static void main(String[] args) {
+        System.out.println("  XXXX  XXXX  ");
+        System.out.println(" X    X    X ");
+        System.out.println(" X         X ");
+        System.out.println(" X  X   X  X ");
+        System.out.println(" X   XXX   X ");
+        System.out.println("  X       X  ");
+        System.out.println("   XXXXXXX   ");
+        System.out.println("     XXX     ");
+    }
+}
